b969c54cea76cdbf77a1415333da32242df33275
Ahmed Alwahishi <ama89999@gmail.com>
2022-11-30 15:48:13 +0300

    other

--- tasks/views.py
+++ tasks/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'tasks/index.html')
+
+def other(request):
+    return render(request,'tasks/other.html')
